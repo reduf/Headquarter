2d6d6df05a7d1e0bbf3b818ee17dcd8d29a3a349
Jon Riley <jon@3vcloud.uk>
2020-04-19 18:21:37 +0100

    Load full plugin pathname relative to current exe

--- code/client/main.c
+++ code/client/main.c
@@ -301,10 +301,14 @@ int main(int argc, const char *argv[])
     #endif
     }
 
-    if (!plugin_load(options.script)) {
-        LogError("Couldn't load the plugin '%s'", options.script);
+    char full_script_path[1024];
+    int length = dlldir(NULL, full_script_path, sizeof(full_script_path));
+    snprintf(&full_script_path[length], sizeof(full_script_path) - length, "/%s", options.script);
+    if (!plugin_load(full_script_path)) {
+        LogError("Couldn't load the plugin '%s'", full_script_path);
         return 1;
     }
+    LogInfo("Plugin loaded, %s\n", full_script_path);
 
     main_loop();
 
