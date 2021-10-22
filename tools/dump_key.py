b4f1755ef3bf0c7b054fa63aedb84c4634795d60
Laurent Dufresne <laurent.dufresne@hotmail.com>
2021-10-22 23:14:49 +0200

    `tools\dump_key.py` now automatically detect version

--- tools/dump_key.py
+++ tools/dump_key.py
@@ -12,7 +12,9 @@ def print_bytes(bytes):
     print(str)
 
 def get_build_number(proc, scanner):
-    raise NotImplementedError("What is the pattern to get the build number?")
+    build_address = scanner.find(b'\xC2\x0C\x00\xCC\xCC\xB8', 6)
+    build_number, = proc.read(build_address, 'I')
+    return build_number
 
 def main(args):
     if args.pid:
@@ -24,28 +26,37 @@ def main(args):
     addr = scanner.find(b'\x8B\x45\x08\xC7\x00\x88\x00\x00\x00\xB8', +0xA)
     keys = proc.read(addr)[0]
 
-    root = proc.read(keys + 4, '4s')[0]
-    prime = proc.read(keys + 8, '64s')[0]
-    public = proc.read(keys + 72, '64s')[0]
+    pr = proc.read(keys + 4, '4s')[0]
+    pm = proc.read(keys + 8, '64s')[0]
+    pk = proc.read(keys + 72, '64s')[0]
 
-    root = reverse_endian(root)
-    prime = reverse_endian(prime)
-    public = reverse_endian(public)
+    pr = int.from_bytes(pr, byteorder='little')
+    pm = int.from_bytes(pm, byteorder='little')
+    pk = int.from_bytes(pk, byteorder='little')
 
     if args.output:
         output = args.output
     else:
         build = get_build_number(proc, scanner)
-        output = utils.get_path(f'data/gw_{build}.pub')
+        output = utils.get_path(f'data\\gw_{build}.pub')
+
+    if args.text:
+        content = '\n'.join([f'pr: {pr}', f'prime: {pm}', f'pkey: {pk}'])
+    else:
+        content = pr.to_bytes(4, byteorder='big')
+        content += pm.to_bytes(64, byteorder='big')
+        content += pk.to_bytes(64, byteorder='big')
 
     if output == '-':
-        fd = sys.stderr.buffer
+        print(content)
     else:
-        fd = open(output, 'wb')
+        print(f'Writting to "{output}"')
+        if args.text:
+            fd = open(output, 'w')
+        else:
+            fd = open(output, 'wb')
+        fd.write(content)
 
-    fd.write(root)
-    fd.write(prime)
-    fd.write(public)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -55,6 +66,8 @@ if __name__ == '__main__':
         help="Process name of the target Guild Wars instance.")
     parser.add_argument("--output", "-o", type=str, required=False,
         help="Path where to write the output keys.")
+    parser.add_argument("--text", action='store_true', required=False,
+        default=False, help="Will output the document as text")
     args = parser.parse_args()
 
     main(args)
