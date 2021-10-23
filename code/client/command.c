020db0681ca101d64253b413de42fdc3497a2abc
Laurent Dufresne <laurent.dufresne@hotmail.com>
2021-10-23 20:47:07 +0200

    Fix usage string

--- code/client/command.c
+++ code/client/command.c
@@ -17,7 +17,7 @@ void print_help(bool terminate)
             "    -password  <string>        Enable auto-loging. use with -email and -charname\n"
             "    -character <string>        Sets the client's security question\n"
             "    -authsrv                   Specify authserver IP to connect to\n\n"
-            ""
+
             "    -newauth                   The client will use Portal connection\n\n"
 
             "    -seed                      Specify the seed for pseudo random generation\n"
@@ -26,17 +26,6 @@ void print_help(bool terminate)
 
             "    -mapid                     Specify the map id you want to start in\n"
             "    -maptype                   Specify the map type you want to start in\\nn"
-
-#ifdef OS_WINDOWS
-            "    Running as service functions:\n"
-            "    -s run                     run as service\n"
-            "    -s install                 install service\n"
-            "    -s uninstall               uninstall service\n"
-#else
-            "    Running as daemon functions:\n"
-            "    -s run                     run as daemon\n"
-            "    -s stop                    stop daemon\n"
-#endif
         );
 
     if (terminate) exit(0);
@@ -77,20 +66,6 @@ void parse_command_args(int argc, const char **argv)
         } else if (!strcmp(arg, "-character")) {
             if (i + 1 >= argc) print_help(true);
             safe_strcpy(options.charname, ARRAY_SIZE(options.charname), argv[++i]);
-        } else if (!strcmp(arg, "-s") || !strcmp(arg, "-service")) {
-            if (i + 1 >= argc) print_help(true);
-
-            const char *cmd = argv[++i];
-            options.launch_service = true;
-
-            if (!strcmp(cmd, "run"))
-                options.service_option = 'r';
-            else if (!strcmp(cmd, "stop"))
-                options.service_option = 's';
-            else if (!strcmp(cmd, "install"))
-                options.service_option = 'i';
-            else if (!strcmp(cmd, "uninstall"))
-                options.service_option = 'u';
         } else if (!strcmp(arg, "-newauth")) {
             options.newauth = true;
         } else if (!strcmp(arg, "-mapid")) {
