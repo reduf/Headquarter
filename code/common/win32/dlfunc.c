09299f6d7cae7b1c5f57398ffbd96711afb84507
Laurent Dufresne <laurent.dufresne@hotmail.com>
2022-02-15 18:06:40 +0100

    Fix build on Windows

--- code/common/win32/dlfunc.c
+++ code/common/win32/dlfunc.c
@@ -15,28 +15,32 @@ void *dllopen(const char *filename)
     return res;
 }
 
-int dllclose(void* handle)
+int dllclose(void *handle)
 {
     if (FreeLibrary((HMODULE)handle))
         return 0;
     else
         return -1;
 }
-int dlllocation(void* handle, char* buffer, int length) {
+
+int dlllocation(void *handle, char *buffer, int length)
+{
     return GetModuleFileName((HMODULE)handle, buffer, length);
 }
-int dlldir(void* handle, char* buffer, int length) {
-    int len = dlllocation(handle, buffer, length);
+
+int dlldir(char *buffer, int length)
+{
+    int len = dlllocation(NULL, buffer, length);
     if (len < 0)
         return len;
-    char* p = strrchr(buffer, '\\');
+    char *p = strrchr(buffer, '\\');
     if (p) p[0] = 0;
     return (int)strlen(buffer);
 }
 
-void *dllsym(void* handle, const char *symbol)
+void *dllsym(void *handle, const char *symbol)
 {
-    void* proc_address = GetProcAddress((HMODULE)handle, symbol);
+    void *proc_address = GetProcAddress((HMODULE)handle, symbol);
     if (proc_address)
         return proc_address;
     printf("GetProcAddress Error: %d findng symbol %s\n", GetLastError(), symbol);
