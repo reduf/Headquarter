7ab4946ede15ead259bd6117d32415d82811faab
Laurent Dufresne <laurent.dufresne@hotmail.com>
2022-05-14 11:05:18 +0200

    Updated for version 37182

--- include/client/Headquarter.h
+++ include/client/Headquarter.h
@@ -1,7 +1,7 @@
 #ifndef HEADQUARTER_H
 #define HEADQUARTER_H
 
-#define GUILD_WARS_VERSION          (37175)
+#define GUILD_WARS_VERSION          (37182)
 #define HEADQUARTER_VERSION_MAJOR   (1)
 #define HEADQUARTER_VERSION_MINOR   (0)
 
