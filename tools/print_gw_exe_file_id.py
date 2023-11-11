ce15ace5b5c05f8384ee582026dcd38f76043dd4
Laurent Dufresne <laurent.dufresne@hotmail.com>
2023-11-11 09:28:04 +0100

    We can now dump keys from a file on disk

--- tools/print_gw_exe_file_id.py
+++ tools/print_gw_exe_file_id.py
@@ -1,30 +1,16 @@
 import os
-import pefile
 import sys
-import struct
-
-# \x8B\xC8\x33\xDB\x39\x8D\xC0\xFD\xFF\xFF\x0F\x95\xC3
+from scanner import FileScanner
 
 def parse_file(path):
     print(f"Processing file '{path}'")
-    parsed = pefile.PE(path, fast_load=True)
-    for section in parsed.sections:
-        if section.Name == b'.text\x00\x00\x00':
-            data = section.get_data()
-            offset = data.find(b'\x8B\xC8\x33\xDB\x39\x8D\xC0\xFD\xFF\xFF\x0F\x95\xC3')
-            if offset == -1:
-                print('Failed to find the pattern in the .text section')
-                os.exit(1)
-            offset -= 5
-            print(f'Found the pattern in the .text section, offset: 0x{offset:X}')
-            call_param, = struct.unpack_from('<I', data, offset + 1)
-            function_offset = call_param + offset + 5
-            print(f'Function offset is: 0x{function_offset:X}')
-            file_id, = struct.unpack_from('<I', data, function_offset + 1)
-            print(f'File id: {file_id}')
-            break
-    else:
-        print('Failed to find the .text section')
+    scanner = FileScanner(path, '.text')
+
+    pos = scanner.find(b'\x8B\xC8\x33\xDB\x39\x8D\xC0\xFD\xFF\xFF\x0F\x95\xC3', -5)
+    function_rva = scanner.follow_call(pos)
+    print(f'Function offset is: 0x{function_rva:X}')
+    file_id, = scanner.read(function_rva + 1, '<I')
+    print(f'File id: {file_id}')
 
 def main(args):
     if len(args) == 0:
